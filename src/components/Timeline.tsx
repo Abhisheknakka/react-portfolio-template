a7c35c1d29217dc1cc07afac61a571841598004e
AbhishekNakka <abhisheknakka15@gmail.com>
2025-03-22 09:36:29 -0400

    code

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -3,6 +3,10 @@ import '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import darwinboxLogo from "../assets/images/darwinbox_logo.png";
 import cognizantLogo from "../assets/images/cognizant_logo.png";
+// You'll need to import a logo for Phoenix Analytics.
+// If you don't have one, you can use a placeholder or find one.
+import phoenixAnalyticsLogo from "../assets/images/phoenix_analytics_logo.jpeg";
+
 
 import { faBriefcase } from '@fortawesome/free-solid-svg-icons';
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
@@ -20,7 +24,7 @@ function Timeline() {
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
             contentStyle={{ background: 'white', color: 'rgb(39, 40, 34)' }}
-            contentArrowStyle={{ borderRight: '7px solid  white' }}
+            contentArrowStyle={{ borderRight: '7px solid  white' }}
             date="Mar 2022 – Dec 2023"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
@@ -28,16 +32,16 @@ function Timeline() {
             <h3 className="vertical-timeline-element-title">Technical Analyst</h3>
             <h4 className="vertical-timeline-element-subtitle">
               <a href="https://www.darwinbox.com/" target="_blank" rel="noreferrer">
-                <img 
-                  src={darwinboxLogo} 
-                  alt="Darwinbox Logo" 
-                  style={{ width: "80px", verticalAlign: "middle", marginRight: "10px" }} 
+                <img
+                  src={darwinboxLogo}
+                  alt="Darwinbox Logo"
+                  style={{ width: "80px", verticalAlign: "middle", marginRight: "10px" }}
                 />
               </a>
-              <a 
-                href="https://www.darwinbox.com/" 
-                target="_blank" 
-                rel="noreferrer" 
+              <a
+                href="https://www.darwinbox.com/"
+                target="_blank"
+                rel="noreferrer"
                 className="visible-link"
               >
                 Darwinbox, Hyderabad, India
@@ -60,16 +64,16 @@ function Timeline() {
             <h3 className="vertical-timeline-element-title">Business Analyst</h3>
             <h4 className="vertical-timeline-element-subtitle">
               <a href="https://www.darwinbox.com/" target="_blank" rel="noreferrer">
-                <img 
-                  src={darwinboxLogo} 
-                  alt="Darwinbox Logo" 
-                  style={{ width: "80px", verticalAlign: "middle", marginRight: "10px" }} 
+                <img
+                  src={darwinboxLogo}
+                  alt="Darwinbox Logo"
+                  style={{ width: "80px", verticalAlign: "middle", marginRight: "10px" }}
                 />
               </a>
-              <a 
-                href="https://www.darwinbox.com/" 
-                target="_blank" 
-                rel="noreferrer" 
+              <a
+                href="https://www.darwinbox.com/"
+                target="_blank"
+                rel="noreferrer"
                 className="visible-link"
               >
                 Darwinbox, Hyderabad, India
@@ -93,16 +97,16 @@ function Timeline() {
             <h3 className="vertical-timeline-element-title">Programmer Analyst</h3>
             <h4 className="vertical-timeline-element-subtitle">
               <a href="https://www.cognizant.com/" target="_blank" rel="noreferrer">
-                <img 
-                  src={cognizantLogo} 
-                  alt="Cognizant Logo" 
-                  style={{ width: "80px", verticalAlign: "middle", marginRight: "10px" }} 
+                <img
+                  src={cognizantLogo}
+                  alt="Cognizant Logo"
+                  style={{ width: "80px", verticalAlign: "middle", marginRight: "10px" }}
                 />
               </a>
-              <a 
-                href="https://www.darwinbox.com/" 
-                target="_blank" 
-                rel="noreferrer" 
+              <a
+                href="https://www.darwinbox.com/"
+                target="_blank"
+                rel="noreferrer"
                 className="visible-link"
               >
                 Cognizant, Hyderabad, India
@@ -112,6 +116,38 @@ function Timeline() {
               Developed scalable data pipelines using PySpark and SQL.
               Automated ETL workflows with Apache Airflow.
               Integrated external data sources using REST APIs.
+            </p>
+          </VerticalTimelineElement>
+
+          {/* Phoenix Analytics - Business Intelligence Analyst */}
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="Jan 2020 – Jun 2021"
+            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
+            icon={<FontAwesomeIcon icon={faBriefcase} />}
+          >
+            <h3 className="vertical-timeline-element-title">Designer & Data Analyst</h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              <a href="https://www.facebook.com/p/Phoenix-Analytics-100064112640425/?locale=ar_AR" target="_blank" rel="noreferrer">
+                <img
+                  src={phoenixAnalyticsLogo}
+                  alt="Phoenix Analytics Logo"
+                  style={{ width: "80px", verticalAlign: "middle", marginRight: "10px" }}
+                />
+              </a>
+              <a
+                href="https://www.facebook.com/p/Phoenix-Analytics-100064112640425/?locale=ar_AR"
+                target="_blank"
+                rel="noreferrer"
+                className="visible-link"
+              >
+                Phoenix Analytics Startup
+              </a>
+            </h4>
+            <p>
+              Developed BI solutions and dashboards to visualize key performance indicators.
+              Conducted data analysis to identify trends and provide actionable insights.
+              Collaborated with stakeholders to define reporting requirements and improve data-driven decision-making.
             </p>
           </VerticalTimelineElement>
 
